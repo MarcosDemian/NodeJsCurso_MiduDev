16ff960704c97a01f660db994c43c5fcb33f93b6
MarcosDemian <demiangarcia254@gmail.com>
2024-07-13 05:02:43 -0300

    Continue class 2
    added activity 3 (express)

---
+++ Clase-2/3.express.js
@@ -0,0 +1,35 @@
+const ditto = require('./pokemon/ditto.json')
+const express = require('express')
+const app = express()
+
+app.disable('x-powered-by')
+
+const PORT = process.env.PORT ?? 1234
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    res.status(201).json(data)
+  })
+})
+
+// la última a la que va a llegar (poner en orden las rutas y al final los errores)
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
