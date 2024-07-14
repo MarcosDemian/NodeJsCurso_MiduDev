b52d14f9d2bf3e5925e4a70a59e2b468bf3a96d1
MarcosDemian <demiangarcia254@gmail.com>
2024-07-13 21:36:48 -0300

    End class 2 & added class 3
    End class 2 & added class 3 (rest api)

--- Clase-2/3.express.js
+++ Clase-2/3.express.js
@@ -6,23 +6,36 @@ app.disable('x-powered-by')
 
 const PORT = process.env.PORT ?? 1234
 
+app.use(express.json()) // hace lo mismo que el codigo de abajo ⬇
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+  // req.body deberíamos guardae en bbdd(base de datos)
+  res.status(201).json(req.body)
 })
 
 // la última a la que va a llegar (poner en orden las rutas y al final los errores)
